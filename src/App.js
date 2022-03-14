8f97f121b46cc2155d2f47e1e5a490bf3f9f8374
Brooke Rahn <brooke.rahn@gmail.com>
2022-03-14 15:50:22 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AllData from "./pages/alldata";
+import CreateAccount from "./pages/createaccount";
+import { Deposit } from "./pages/deposit";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Withdraw from "./pages/withdraw";
+import NavBar from "./components/navbar";
+import { UserContext } from "./context/context";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <UserContext.Provider
+          value={{
+            user: null,
+            users: [
+              {
+                name: "Brooke",
+                email: "brooke.rahn@gmail.com",
+                password: "secret",
+                balance: 100,
+              },
+            ],
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/CreateAccount" element={<CreateAccount />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/deposit" element={<Deposit />} />
+            <Route path="/withdraw" element={<Withdraw />} />
+            <Route path="/alldata" element={<AllData />} />
+          </Routes>
+        </UserContext.Provider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/deposit.js
@@ -0,0 +1,162 @@
+import Card from "../components/card";
+import { Formik, Form, Field, ErrorMessage, validateYupSchema } from "formik";
+import * as Yup from "yup";
+import { useState, useContext } from "react";
+import { UserContext } from "../context/context";
+function DepositForm() {
+  const ctx = useContext(UserContext);
+  const [balance, setBalance] = useState(
+    ctx.user ? ctx.user.balance : "User is Not Logged In"
+  );
+  const initialValues = {
+    amount: 0,
+  };
+  const validationSchema = Yup.object().shape({
+    amount: Yup.number()
+      .positive("You must enter a positive number")
+      .required("You must enter a number"),
+  });
+  function onSubmit(amount) {
+    if (ctx.user) {
+      ctx.user.balance = Number(ctx.user.balance) + Number(amount.amount);
+      setBalance(ctx.user.balance);
+    }
+  }
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(props) => {
+        return (
+          <Form>
+            <label htmlFor="balance" className="form-label">
+              Balance
+            </label>
+            <input
+              readOnly
+              className="form-control-plaintext"
+              value={balance}
+              type="text"
+              id="balance"
+            />
+
+            <label htmlFor="amount" className="form-label">
+              Deposit
+            </label>
+            <Field
+              className="form-control"
+              type="number"
+              name="amount"
+              id="amount"
+            />
+            <ErrorMessage
+              component={"div"}
+              name="amount"
+              className="form-text ms-2 text-danger"
+            ></ErrorMessage>
+
+            <input
+              type="submit"
+              disabled={!ctx.user || props.values.amount <= 0}
+              className="btn btn-light mt-3"
+              value="Deposit"
+            />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+}
+function Deposit() {
+  const [show, setShow] = useState(true);
+  const ctx = useContext(UserContext);
+  const [balance, setBalance] = useState(
+    ctx.user ? ctx.user.balance : "User is Not Logged In"
+  );
+  const initialValues = {
+    amount: 0,
+  };
+  const validationSchema = Yup.object().shape({
+    amount: Yup.number()
+      .positive("You must enter a positive number")
+      .required("You must enter a number"),
+  });
+  function onSubmit(amount) {
+    if (ctx.user) {
+      ctx.user.balance = Number(ctx.user.balance) + Number(amount.amount);
+      setBalance(ctx.user.balance);
+      setShow(false);
+    }
+  }
+  function clearForm() {
+    setShow(true);
+  }
+  return (
+    <Card
+      txtcolor="black"
+      bgcolor="success"
+      header="BadDeposit"
+      title="Deposit"
+      text="Enter an amount to deposit."
+      body={
+        show ? (
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {(props) => {
+              return (
+                <Form>
+                  <label htmlFor="balance" className="form-label">
+                    Balance
+                  </label>
+                  <input
+                    readOnly
+                    className="form-control-plaintext"
+                    value={balance}
+                    type="text"
+                    id="balance"
+                  />
+
+                  <label htmlFor="amount" className="form-label">
+                    Deposit
+                  </label>
+                  <Field
+                    className="form-control"
+                    type="number"
+                    name="amount"
+                    id="amount"
+                  />
+                  <ErrorMessage
+                    component={"div"}
+                    name="amount"
+                    className="form-text ms-2 text-danger"
+                  ></ErrorMessage>
+
+                  <input
+                    type="submit"
+                    disabled={!ctx.user || props.values.amount <= 0}
+                    className="btn btn-light mt-3"
+                    value="Deposit"
+                  />
+                </Form>
+              );
+            }}
+          </Formik>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <h6>Deposit Completed</h6>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>
+              Make Another Deposit
+            </button>
+          </>
+        )
+      }
+    />
+  );
+}
+export { Deposit, DepositForm };
